9e6e6946f0e44d116c77153255dd9e3c03f11622
Raghib Hasan <raghib.hasan@codemate.com>
2018-12-22 20:08:56 +0200

    fix prisma env stuffs

--- server/src/generated/prisma-client/index.js
+++ server/src/generated/prisma-client/index.js
@@ -20,21 +20,7 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: ``,
-  secret: `mysecret123`
+  endpoint: `${process.env["PRISMA_ENDPOINT"]}`,
+  secret: `${process.env["PRISMA_SECRET"]}`
 });
 exports.prisma = new exports.Prisma();
-var models = [
-  {
-    name: "Link",
-    embedded: false
-  },
-  {
-    name: "User",
-    embedded: false
-  },
-  {
-    name: "Vote",
-    embedded: false
-  }
-];
